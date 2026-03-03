255c4a092b4916c3ebec29ffa1b536a09fd82883
CristianS-2006 <cristianandressantos5@gmail.com>
2026-03-03 11:43:59 -0500

    feat: add backend project files

---
+++ Backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TutorViewSet, MateriaViewSet, TutoriaViewSet
+
+router = DefaultRouter()
+router.register(r'tutores', TutorViewSet)
+router.register(r'materias', MateriaViewSet)
+router.register(r'tutorias', TutoriaViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
