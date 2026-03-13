17ec79cbef2f2ae938f8a10ddddf7cbdfcd94094
Cristian <cristianandressantos5@gmail.com>
2026-03-13 15:51:51 -0500

    Definición de modelos de base de datos para tutorías

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+# Modelos de la base de datos para el sistema de tutorías
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
