fb9d6234ed9175f3d8807a92aaec36bfdc88fb53
Cristian <cristianandressantos5@gmail.com>
2026-04-14 10:11:23 -0500

    chore: renombrar carpeta Frontend a web y organizar archivos

---
+++ web/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+/**
+ * Punto de montaje: solo delega en el router (layout y vistas en rutas).
+ */
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView />
+</template>
